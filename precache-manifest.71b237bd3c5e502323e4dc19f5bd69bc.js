05cc83d3891eda3b22afcbd284550529507eace9
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-05-26 12:36:39 +0300

    Updates

---
+++ precache-manifest.71b237bd3c5e502323e4dc19f5bd69bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6383355341eee29fddcf446c4e0bee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17bef0f2fc35da65a1ff",
+    "url": "./static/css/main.dd7f6d95.chunk.css"
+  },
+  {
+    "revision": "4bf1f1228cbc518f23c3",
+    "url": "./static/js/2.78ed7ac1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.78ed7ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17bef0f2fc35da65a1ff",
+    "url": "./static/js/main.177ba347.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
